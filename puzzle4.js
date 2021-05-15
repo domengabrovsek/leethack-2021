76299ca2e52bf7d8634e7099bbc578a985356337
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2021-05-15 12:47:38 +0200

    Puzzle 4

---
+++ puzzle4.js
@@ -0,0 +1,239 @@
+const input = `<gig>
+<band>Metallocate</band>
+<data>1967-07-12 22:15:00</data>
+<songs>
+  <song willPlay="true">Die() Die() my darling</song>
+  <song willPlay="false">One Zero</song>
+  <song willPlay="true">The call of the compiler</song>
+  <song willPlay="true">Am I eval (cover)</song>
+  <song willPlay="true">Damage++!</song>
+  <song willPlay="true">Slackitude</song>
+  <song willPlay="true">Creeping crash</song>
+  <song willPlay="false">Slackitude</song>
+  <song willPlay="false">Eye of the container</song>
+  <song willPlay="true">Creeping crash</song>
+  <song willPlay="false">Don't thread on me</song>
+  <song willPlay="false">To thread is to die</song>
+  <song willPlay="true">Damage++!</song>
+  <song willPlay="true">False but true</song>
+  <song willPlay="true">goto fire</song>
+  <song willPlay="true">Harvester of garbage</song>
+  <song willPlay="true">Of react and redux</song>
+  <song willPlay="false">Creeping crash</song>
+  <song willPlay="true">Eye of the container</song>
+  <song willPlay="true">King undefined</song>
+  <song willPlay="true">Harvester of garbage</song>
+  <song willPlay="false">For whom the Krell tolls</song>
+  <song willPlay="false">Eye of the container</song>
+  <song willPlay="false">King undefined</song>
+  <song willPlay="false">Slackitude</song>
+  <song willPlay="true">Slackitude</song>
+  <song willPlay="true">The memory reallocates</song>
+  <song willPlay="false">Harvester of garbage</song>
+  <song willPlay="true">The frayed ends of sanity checks</song>
+  <song willPlay="true">One Zero</song>
+  <song willPlay="true">And justice for (.*+)</song>
+  <song willPlay="true">The memory reallocates</song>
+  <song willPlay="true">For whom the Krell tolls</song>
+  <song willPlay="true">Whiskey.jar</song>
+  <song willPlay="false">Creeping crash</song>
+  <song willPlay="false">The frayed ends of sanity checks</song>
+  <song willPlay="false">Die() Die() my darling</song>
+  <song willPlay="false">Whiskey.jar</song>
+  <song willPlay="true">Die() Die() my darling</song>
+  <song willPlay="true">Damage++!</song>
+  <song willPlay="true">Scrum master of puppets</song>
+  <song willPlay="true">Carpal tunnel baby</song>
+  <song willPlay="true">Creeping crash</song>
+  <song willPlay="true">The frayed ends of sanity checks</song>
+  <song willPlay="true">The frayed ends of sanity checks</song>
+  <song willPlay="true">It's eclectic (cover)</song>
+  <song willPlay="true">Creeping crash</song>
+  <song willPlay="false">The frayed ends of sanity checks</song>
+  <song willPlay="true">Harvester of garbage</song>
+  <song willPlay="true">For whom the Krell tolls</song>
+  <song willPlay="false">One Zero</song>
+  <song willPlay="true">For whom the Krell tolls</song>
+  <song willPlay="true">goto fire</song>
+  <song willPlay="true">Until sleep(0)</song>
+  <song willPlay="false">One Zero</song>
+  <song willPlay="false">Scrum master of puppets</song>
+  <song willPlay="false">For whom the Krell tolls</song>
+  <song willPlay="true">Until sleep(0)</song>
+  <song willPlay="true">The memory reallocates</song>
+  <song willPlay="false">And justice for (.*+)</song>
+  <song willPlay="true">Scrum master of puppets</song>
+  <song willPlay="true">Scrum master of puppets</song>
+  <song willPlay="true">Astrology (cover)</song>
+  <song willPlay="true">False but true</song>
+  <song willPlay="true">One Zero</song>
+  <song willPlay="true">The frayed ends of sanity checks</song>
+  <song willPlay="true">Where the wingdings are</song>
+  <song willPlay="true">Scrum master of puppets</song>
+  <song willPlay="false">Of react and redux</song>
+  <song willPlay="true">Until sleep(0)</song>
+  <song willPlay="false">Don't thread on me</song>
+  <song willPlay="true">Astrology (cover)</song>
+  <song willPlay="true">Whiskey.jar</song>
+  <song willPlay="true">False but true</song>
+  <song willPlay="true">Creeping crash</song>
+  <song willPlay="true">One Zero</song>
+  <song willPlay="true">Whiskey.jar</song>
+  <song willPlay="true">To thread is to die</song>
+  <song willPlay="false">Am I eval (cover)</song>
+  <song willPlay="false">Scrum master of puppets</song>
+  <song willPlay="true">The call of the compiler</song>
+  <song willPlay="true">False but true</song>
+  <song willPlay="true">To thread is to die</song>
+  <song willPlay="true">Am I eval (cover)</song>
+  <song willPlay="true">Don't thread on me</song>
+  <song willPlay="true">Carpal tunnel baby</song>
+  <song willPlay="true">Whiskey.jar</song>
+  <song willPlay="true">The memory reallocates</song>
+  <song willPlay="true">Of react and redux</song>
+  <song willPlay="false">King undefined</song>
+  <song willPlay="true">Harvester of garbage</song>
+  <song willPlay="true">To thread is to die</song>
+  <song willPlay="true">One Zero</song>
+  <song willPlay="true">Damage++!</song>
+  <song willPlay="false">Eye of the container</song>
+  <song willPlay="false">Harvester of garbage</song>
+  <song willPlay="false">Don't thread on me</song>
+  <song willPlay="true">I/O Disappear</song>
+  <song willPlay="false">Harvester of garbage</song>
+  <song willPlay="true">King undefined</song>
+  <song willPlay="true">To thread is to die</song>
+  <song willPlay="false">I/O Disappear</song>
+  <song willPlay="true">Harvester of garbage</song>
+  <song willPlay="false">The memory reallocates</song>
+  <song willPlay="true">Creeping crash</song>
+  <song willPlay="false">It's eclectic (cover)</song>
+  <song willPlay="true">Am I eval (cover)</song>
+  <song willPlay="false">The call of the compiler</song>
+  <song willPlay="true">King undefined</song>
+  <song willPlay="true">I/O Disappear</song>
+  <song willPlay="true">For whom the Krell tolls</song>
+  <song willPlay="true">The frayed ends of sanity checks</song>
+  <song willPlay="true">Until sleep(0)</song>
+  <song willPlay="true">False but true</song>
+  <song willPlay="true">I/O Disappear</song>
+  <song willPlay="false">Until sleep(0)</song>
+  <song willPlay="false">goto fire</song>
+  <song willPlay="true">Harvester of garbage</song>
+  <song willPlay="true">Where the wingdings are</song>
+  <song willPlay="false">I/O Disappear</song>
+  <song willPlay="false">Harvester of garbage</song>
+  <song willPlay="true">Creeping crash</song>
+  <song willPlay="false">And justice for (.*+)</song>
+  <song willPlay="true">It's eclectic (cover)</song>
+  <song willPlay="false">Slackitude</song>
+  <song willPlay="true">To thread is to die</song>
+  <song willPlay="true">One Zero</song>
+  <song willPlay="true">Carpal tunnel baby</song>
+  <song willPlay="false">Am I eval (cover)</song>
+  <song willPlay="true">Of react and redux</song>
+  <song willPlay="true">Carpal tunnel baby</song>
+  <song willPlay="false">False but true</song>
+  <song willPlay="true">One Zero</song>
+  <song willPlay="false">For whom the Krell tolls</song>
+  <song willPlay="true">I/O Disappear</song>
+  <song willPlay="true">Of react and redux</song>
+  <song willPlay="false">I/O Disappear</song>
+  <song willPlay="true">To thread is to die</song>
+  <song willPlay="false">King undefined</song>
+  <song willPlay="false">False but true</song>
+  <song willPlay="false">Harvester of garbage</song>
+  <song willPlay="true">Slackitude</song>
+  <song willPlay="true">One Zero</song>
+  <song willPlay="true">Harvester of garbage</song>
+  <song willPlay="true">Of react and redux</song>
+  <song willPlay="true">Die() Die() my darling</song>
+  <song willPlay="true">It's eclectic (cover)</song>
+  <song willPlay="true">Of react and redux</song>
+  <song willPlay="true">King undefined</song>
+  <song willPlay="false">To thread is to die</song>
+  <song willPlay="false">For whom the Krell tolls</song>
+  <song willPlay="false">Where the wingdings are</song>
+  <song willPlay="true">For whom the Krell tolls</song>
+  <song willPlay="false">False but true</song>
+  <song willPlay="true">Until sleep(0)</song>
+  <song willPlay="true">The frayed ends of sanity checks</song>
+  <song willPlay="false">The memory reallocates</song>
+  <song willPlay="true">The call of the compiler</song>
+  <song willPlay="false">I/O Disappear</song>
+  <song willPlay="false">Carpal tunnel baby</song>
+  <song willPlay="true">One Zero</song>
+  <song willPlay="false">Die() Die() my darling</song>
+  <song willPlay="false">The call of the compiler</song>
+  <song willPlay="true">For whom the Krell tolls</song>
+  <song willPlay="true">To thread is to die</song>
+  <song willPlay="true">Eye of the container</song>
+  <song willPlay="true">Where the wingdings are</song>
+  <song willPlay="true">Where the wingdings are</song>
+  <song willPlay="true">For whom the Krell tolls</song>
+  <song willPlay="true">Slackitude</song>
+  <song willPlay="true">Of react and redux</song>
+  <song willPlay="false">Am I eval (cover)</song>
+  <song willPlay="true">The call of the compiler</song>
+  <song willPlay="true">False but true</song>
+  <song willPlay="false">King undefined</song>
+  <song willPlay="false">The memory reallocates</song>
+  <song willPlay="true">I/O Disappear</song>
+  <song willPlay="true">Creeping crash</song>
+  <song willPlay="false">False but true</song>
+  <song willPlay="false">Slackitude</song>
+  <song willPlay="true">Astrology (cover)</song>
+  <song willPlay="true">It's eclectic (cover)</song>
+  <song willPlay="true">Damage++!</song>
+  <song willPlay="true">Slackitude</song>
+  <song willPlay="true">It's eclectic (cover)</song>
+  <song willPlay="true">The call of the compiler</song>
+  <song willPlay="false">goto fire</song>
+  <song willPlay="false">Of react and redux</song>
+  <song willPlay="true">It's eclectic (cover)</song>
+  <song willPlay="false">Harvester of garbage</song>
+  <song willPlay="false">The frayed ends of sanity checks</song>
+  <song willPlay="true">Am I eval (cover)</song>
+  <song willPlay="true">Creeping crash</song>
+  <song willPlay="true">Whiskey.jar</song>
+  <song willPlay="true">Don't thread on me</song>
+  <song willPlay="true">Carpal tunnel baby</song>
+  <song willPlay="true">The call of the compiler</song>
+  <song willPlay="true">King undefined</song>
+  <song willPlay="false">For whom the Krell tolls</song>
+  <song willPlay="true">The memory reallocates</song>
+</songs>
+</gig>`;
+
+
+var parseString = require('xml2js').parseString;
+
+let interestedSongs = [];
+
+const stuff = parseString(input, (error, result) => {
+
+  const songs = result.gig.songs[0];
+
+  songs.song.forEach(song => {
+    if (song.$.willPlay === 'true') {
+      interestedSongs.push(song._)
+    }
+  })
+})
+
+const dict = {}
+
+interestedSongs.forEach(song => {
+  if (!dict[song]) {
+    dict[song] = 1;
+  } else {
+    dict[song]++;
+  }
+})
+
+const keys = Object.keys(dict).sort();
+
+keys.forEach(key => {
+  console.log(`${key}, ${dict[key]}`)
+})
+
